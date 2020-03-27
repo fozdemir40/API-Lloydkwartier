cdeeeea7d68989089a6af25caab44e1ad5cd1586
Furkan Ozdemir <f.ozdemir40@hotmail.com>
2020-03-27 04:38:48 +0100

    Add routes for moves object

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('Lobby', views.LobbyView)
+router.register('Moves', views.MovesView)
 
 urlpatterns = [
     path('', include(router.urls))
--- api/views.py
+++ api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Lobby, Moves
-from .serializers import LobbySerializer
+from .serializers import LobbySerializer, MovesSerializer
+
 
 # Create your views here.
-class LobbyView (viewsets.ModelViewSet):
+class LobbyView(viewsets.ModelViewSet):
     queryset = Lobby.objects.all()
     serializer_class = LobbySerializer
 
 
+class MovesView(viewsets.ModelViewSet):
+    queryset = Moves.objects.all()
+    serializer_class = MovesSerializer
